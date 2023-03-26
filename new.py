c3bb1f4d0015a682de3195ba7b8d8f89fe5dda8e
himanshushah05 <78071859+himanshushah05@users.noreply.github.com>
2023-03-27 00:08:10 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO 
+from time import sleep 
+GPIO.setmode (GPIO. BCM)
+pwmPin=18
+GPIO.setup(pwmPin, GPIO.OUT)
+pwm=GPIO.PWM (pwmPin, 50)
+pwm.start(0)
+try:
+  while True:
+    pwmPercent-float (input('PWM %')) 
+    pwm.ChangeDutyCycle (pwmPercent)
+    sleep(.1)
+except KeyboardInterrupt:
+  GPIO.cleanup()
